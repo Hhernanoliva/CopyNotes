34104f61d30429a65ad9c5f805563f7fe215e77f
Hernán Oliva <hhernanoliva@gmail.com>
2026-07-11 20:23:14 -0300

    feat(format): color catalog, engine barrel, css tokens

---
+++ src/lib/format/index.ts
@@ -0,0 +1,7 @@
+export { sanitizeHtml, htmlToPlainText } from './sanitize';
+export { normalizeUrl } from './url';
+export { activeFormatsFor } from './active';
+export { commandsForSelection } from './safety';
+export { HEADING_TYPES, planBlockType } from './blocktype';
+export { TEXT_COLORS } from './colors';
+export { applyInline, removeInline, applyColor, applyLink, removeLink } from './commands';
